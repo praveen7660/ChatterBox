a493893c4f2b8c0129063687af571367da3b303a
Praveen <prvn07tiwari@gmail.com>
2024-12-28 20:58:55 +0530

    added message route

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -33,3 +33,35 @@ export const getMessages = async(req,res)=>{
         res.status(500).json({message:"Internal Server Error"})
     }
 };
+
+export const sendMessage = async(req,res)=>{
+    try{
+        const { text, image } = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            //uploading base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        //todo the realtime functionality with Socket.io
+
+        res.status(201).json(newMessage);
+    }
+    catch(err){
+        console.log("Error in sendMessage controller: ",err.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+};
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,8 +1,10 @@
 import express from "express"
 import { protectedRoute } from "../middlewares/auth.middleware.js";
-import { getUserForSidebar,getMessages } from "../controllers/message.controller.js";
+import { getUserForSidebar,getMessages, sendMessage } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users",protectedRoute,getUserForSidebar)
 router.get("/:id",protectedRoute,getMessages)
+
+router.post("/send/:id",protectedRoute,sendMessage)
 export default router;
